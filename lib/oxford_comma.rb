7987574c141afface3f201ff30a0c7f67b3d69e7
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-21 21:18:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   if l == 1
     string << array[0]
   elsif l == 2
-    string += array.join(" and ")
+    string << array.join(" and ")
   else
     string += array.slice(0, l-1).join(", ") + "and " + array.last
   end
